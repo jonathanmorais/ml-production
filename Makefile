3f753ddead9d47e44730121d8c713eba444bc9e2
jonathanmorais <jonathan.m.lucena@gmail.com>
2022-04-06 12:15:02 -0300

    tests

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	cd infra/ && docker build -t iris-ml-image  -f Dockerfile ..
 
 run:
-	docker run -it --rm -p 8080:8080 iris-ml-image
+	docker run -it --name iris_model --network host --rm -p 8080:8080 iris-ml-image
 
 rm:
 	docker rm -f iris-ml-image
---
+++ tests/test_prediction.py
@@ -0,0 +1,26 @@
+from fastapi_skeleton.core import config
+
+
+def test_prediction(test_client) -> None:
+    response = test_client.post(
+        "/api/model/predict",
+        json={
+            "sepal_l": 5.8,
+            "sepal_w": 2.7,
+            "petal_l": 5.1,
+            "petal_w": 1.9
+        },
+        headers={"token": str(config.API_KEY)}
+    )
+    assert response.status_code == 200
+    assert "median_house_value" in response.json()
+    assert "currency" in response.json()
+
+
+def test_prediction_nopayload(test_client) -> None:
+    response = test_client.post(
+        "/api/classification/predict",
+        json={},
+        headers={"token": str(config.API_KEY)}
+    )
+    assert response.status_code == 422
